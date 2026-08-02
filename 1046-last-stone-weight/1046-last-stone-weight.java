574a594485e56b34fe4bff621930df92ed555ce1
Abhinav S P <spabhinav919@gmail.com>
2026-08-02 09:53:27 +0530

    Time: 1 ms (99.62%), Space: 42.3 MB (99.22%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> maxHeap =
+                new PriorityQueue<>(Collections.reverseOrder());
+
+        // Add all stones
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        // Smash stones
+        while (maxHeap.size() > 1) {
+
+            int y = maxHeap.poll(); // largest
+            int x = maxHeap.poll(); // second largest
+
+            if (y != x) {
+                maxHeap.offer(y - x);
+            }
+        }
+
+        return maxHeap.isEmpty() ? 0 : maxHeap.peek();
+    }
+}
