292da80879134f475dd96cdeba1a2a81141cdb63
Abhinav S P <spabhinav919@gmail.com>
2026-08-23 12:26:52 +0530

    Time: 1 ms (14.35%), Space: 42.2 MB (48.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,28 +1,43 @@
+import java.util.*;
+
 class Solution {
 
     public int uniquePaths(int m, int n) {
 
         int[][] dp = new int[m][n];
 
-        // First row
-        for(int j = 0; j < n; j++) {
-            dp[0][j] = 1;
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        // First column
-        for(int i = 0; i < m; i++) {
-            dp[i][0] = 1;
+        return paths(0, 0, m, n, dp);
+    }
+
+    static int paths(int i, int j, int m, int n, int[][] dp) {
+
+        // Reached destination
+        if (i == m - 1 && j == n - 1) {
+            return 1;
         }
 
-        // Fill remaining cells
-        for(int i = 1; i < m; i++) {
-
-            for(int j = 1; j < n; j++) {
-
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
+        // Outside grid
+        if (i >= m || j >= n) {
+            return 0;
         }
 
-        return dp[m-1][n-1];
+        // Already calculated
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        // Move down
+        int down = paths(i + 1, j, m, n, dp);
+
+        // Move right
+        int right = paths(i, j + 1, m, n, dp);
+
+        dp[i][j] = down + right;
+
+        return dp[i][j];
     }
 }
