ef338e0b5a5ba69e3e21334f744497b4876addae
Abhinav S P <spabhinav919@gmail.com>
2026-08-23 16:18:09 +0530

    Time: 0 ms (100%), Space: 43.4 MB (65.04%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -7,46 +7,44 @@ class Solution {
 
         int[][] dp = new int[m][n];
 
-        // Starting cell is blocked
-        if (grid[0][0] == 1) {
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return paths(0, 0, grid, dp);
+    }
+
+    static int paths(int i, int j, int[][] grid, int[][] dp) {
+
+        // Outside the grid
+        if (i >= grid.length || j >= grid[0].length) {
             return 0;
         }
 
-        dp[0][0] = 1;
-
-        // First row
-        for (int j = 1; j < n; j++) {
-
-            if (grid[0][j] == 0) {
-                dp[0][j] = dp[0][j - 1];
-            }
+        // Obstacle
+        if (grid[i][j] == 1) {
+            return 0;
         }
 
-        // First column
-        for (int i = 1; i < m; i++) {
-
-            if (grid[i][0] == 0) {
-                dp[i][0] = dp[i - 1][0];
-            }
+        // Reached destination
+        if (i == grid.length - 1 &&
+            j == grid[0].length - 1) {
+            return 1;
         }
 
-        // Remaining cells
-        for (int i = 1; i < m; i++) {
-
-            for (int j = 1; j < n; j++) {
-
-                // Obstacle
-                if (grid[i][j] == 1) {
-                    dp[i][j] = 0;
-                }
-                else {
-                    dp[i][j] =
-                        dp[i - 1][j] +
-                        dp[i][j - 1];
-                }
-            }
+        // Already calculated
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
 
-        return dp[m - 1][n - 1];
+        // Move down
+        int down = paths(i + 1, j, grid, dp);
+
+        // Move right
+        int right = paths(i, j + 1, grid, dp);
+
+        dp[i][j] = down + right;
+
+        return dp[i][j];
     }
 }
